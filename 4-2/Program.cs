c269efae8685557cd3c8c0940366b00e3f83c152
Denis <9544380@mail.ru>
2023-08-31 20:05:16 +0300

    2

---
+++ 4-2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// System.Console.WriteLine("Введите число A");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// while (A > 0)
+// {
+//     int result = A % 10;
+//     sum = sum + result;
+//     A = A / 10;
+// }
+
+// System.Console.WriteLine(sum);
+
+
+int SumDigits(int upper)
+{
+    int result = 0;
+    if (upper < 0)
+        upper = -upper;
+    while (upper > 0)
+    {
+        int rest = upper % 10;
+        result = result + rest;
+        upper = upper / 10;
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите число ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Cумма цифр в цисле {A} равно {SumDigits(A)}");
