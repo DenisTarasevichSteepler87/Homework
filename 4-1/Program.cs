55d34a8ec77496c59ad73fea5b726bcc5af77f0f
Denis <9544380@mail.ru>
2023-08-31 19:58:42 +0300

    1

--- 4-1/Program.cs
+++ 4-1/Program.cs
@@ -1,2 +1,33 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3?)
+// 2, 4 -> 16
+
+
+// System.Console.WriteLine("Введите число A");
+// int A = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите натуральную степень B");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int result = 1;
+// for (int i = 0; i < B; i++)
+//     result = result * A;
+
+// System.Console.WriteLine(result);
+
+
+
+
+
+int Degree(int upper1, int upper2)
+{
+    int result = 1;
+    for (int i = 0; i < upper2; i++)
+        result = result * upper1;
+    return result;
+}
+
+System.Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите натуральную степень B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Число {A} возведенное в натуральную степень {B} равно {Degree(A, B)}");
