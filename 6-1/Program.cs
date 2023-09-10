7457ce83880624c4932fc4440ff4274d13004339
Denis <9544380@mail.ru>
2023-09-10 14:04:48 +0300

    10

---
+++ 6-1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[] InputArray(int length)
+{
+int[] array = new int[length];
+for (int i= 0; i < array.Length; i++)
+{
+    array[i] = Prompt($"Введите {i+1}-й элемент");
+}
+return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"a[{i}]= {array[i]}");
+    }
+}
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int lenght = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray(lenght);
+PrintArray(array);
+System.Console.WriteLine($" Количество чисел больше 0 - {CountPositiveNumbers(array)}");
